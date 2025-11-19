3a1b10beaa679b16839ba3e88a75bc37ab0406d6
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-11-19 10:56:24 +0100

    getAllAktiehandler er funktionel

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
+const baseURL='http://localhost:5211/api/AktieHandel'
+
 const app = Vue.createApp({
     data() {
         return {
-            title:'Velkommen til min Tamplate',
-            indhold:['v-for','v-if','v-on:click','v-Show'],
-            showIndhold:true
+            title:'Aktiehandler',
+            allActiehandler:[],
+            filtherSortedAktiehandler:[],
+            aktieID:0,
+            aktieNavn:'',
+            antal:0,
+            aktiHandelsPris:0,
+            statusCode:''
+
         }
     },
     methods: {
-        ChangeShowIndholdStatus(){
-            if(this.showIndhold==true){
-                this.showIndhold=false
-            }
-            else{
-                this.showIndhold=true
-            }
-                
+        getAllAktiehandler(){
+            console.log('Er i getAll')
+
+            axios.get(baseURL).then(response =>{
+                this.allActiehandler = response.data
+                this.filtherSortedAktiehandler=this.allActiehandler
+
+                this.statusCode= response.status
+            })
+            .catch(
+                error=> {
+                    console.log(error)
+                }
+            )
         }
     }
 })
